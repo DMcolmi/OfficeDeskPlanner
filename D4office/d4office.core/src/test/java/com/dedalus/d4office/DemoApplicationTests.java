72eb85707d1689672ac786a23c310a1745994c61
davide <davidemaria.colmi@gmail.com>
2022-11-13 18:15:10 +0100

    core created

---
+++ D4office/d4office.core/src/test/java/com/dedalus/d4office/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dedalus.d4office;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
