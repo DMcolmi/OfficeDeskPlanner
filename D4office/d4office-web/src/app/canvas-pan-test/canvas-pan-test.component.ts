c4e3ddd98004d5664fca0ee5f9e3b593dd6d5c50
davide <davidemaria.colmi@gmail.com>
2022-11-29 18:16:18 +0100

    pan test

--- D4office/d4office-web/src/app/canvas-pan-test/canvas-pan-test.component.ts
+++ D4office/d4office-web/src/app/canvas-pan-test/canvas-pan-test.component.ts
@@ -10,28 +10,30 @@ export class CanvasPanTestComponent implements OnInit {
 
   @ViewChild('canvasPan', { static: true })
   canvasPan: ElementRef<HTMLCanvasElement>;
+  canvas: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;  
   cameraOffset: {x: number, y: number};
   previousCameraOffset: {x: number, y: number};
   isDragging = false;  
   dragStart: {x: number, y: number} = {x: 0, y: 0};
   cameraZoom = 1;
-  
+
   constructor() {
   }
 
   ngOnInit(): void {
-    this.ctx = this.canvasPan.nativeElement.getContext("2d")!;
-    this.canvasPan.nativeElement.width = window.innerWidth;
-    this.canvasPan.nativeElement.height = window.innerHeight;
+    this.canvas = this.canvasPan.nativeElement;
+    this.ctx = this.canvas.getContext("2d")!;
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
     //this.cameraOffset =  { x: window.innerWidth/2, y: window.innerHeight/2 };
     this.cameraOffset =  { x: 0, y: 0 };
     this.previousCameraOffset =  { x: 0, y: 0 };
 
 
-    this.canvasPan.nativeElement.addEventListener('mousedown', (e: MouseEvent) => this.onPonterDown(e));
-    this.canvasPan.nativeElement.addEventListener('mouseup', (e: MouseEvent) => this.onPointerUp(e));
-    this.canvasPan.nativeElement.addEventListener('mousemove', (e: MouseEvent) => this.onPointerMove(e));
+    this.canvas.addEventListener('mousedown', (e: MouseEvent) => this.onPonterDown(e));
+    this.canvas.addEventListener('mouseup', (e: MouseEvent) => this.onPointerUp(e));
+    this.canvas.addEventListener('mousemove', (e: MouseEvent) => this.onPointerMove(e));
 
     this.draw();
   }
@@ -39,11 +41,17 @@ export class CanvasPanTestComponent implements OnInit {
   public draw() { 
     
     //this.ctx.translate( window.innerWidth / 2, window.innerHeight / 2 );
-    //this.ctx.scale(this.cameraZoom, this.cameraZoom);
+    this.ctx.scale(this.cameraZoom, this.cameraZoom);
     //this.ctx.translate( -window.innerWidth / 2 + this.cameraOffset.x, -window.innerHeight / 2 + this.cameraOffset.y );
-    this.previousCameraOffset.x = this.cameraOffset.x;
-    this.previousCameraOffset.y = this.cameraOffset.y;
-    this.ctx.translate(this.cameraOffset.x, this.cameraOffset.y );    
+    
+    {
+      if(this.cameraOffset.x != 0 && this.cameraOffset.y !=0)
+        this.ctx.translate(this.cameraOffset.x -this.previousCameraOffset.x , this.cameraOffset.y -this.previousCameraOffset.y);
+      
+       this.previousCameraOffset.x = this.cameraOffset.x;
+       this.previousCameraOffset.y = this.cameraOffset.y;
+    }
+
     this.cameraOffset.x = 0;
     this.cameraOffset.y = 0;
     
@@ -51,44 +59,35 @@ export class CanvasPanTestComponent implements OnInit {
     this.ctx.clearRect(0,0,window.innerWidth, window.innerHeight);
 
     //parte che disegna    
-    this.ctx.fillStyle = "#991111";
-    this.ctx.fillRect(100, 100, 100, 100);
+    this.ctx.font = "50px Verdana";
+    this.ctx.fillText("Big smile! Move me!", 100, 100);
     
     //fine parte che disegna
     requestAnimationFrame(() => this.draw());    
   }
 
   onPonterDown(e: MouseEvent){
+
     this.isDragging = true;
-    this.dragStart.x = getEventLocation(e).x - this.cameraOffset.x;
-    this.dragStart.y = getEventLocation(e).y - this.cameraOffset.y;
-    
-    //this.dragStart.x = getEventLocation(e).x;
-    //this.dragStart.y = getEventLocation(e).y;
-    
-    console.log('drag start: ', this.dragStart.x , this.dragStart.y);    
+    this.dragStart.x = getEventLocation(e, this.canvas).x;
+    this.dragStart.y = getEventLocation(e, this.canvas).y;
   }
 
   onPointerUp(e: MouseEvent){
-    this.isDragging = false;
-    console.log(this.isDragging);
-    console.log('drag end: ', this.cameraOffset.x , this.cameraOffset.y);
+    this.isDragging = false;    
   }
 
   onPointerMove(e: MouseEvent){
     if(this.isDragging){
-      console.log('drag start: ', this.dragStart.x , this.dragStart.y);
-      console.log('previous:', this.previousCameraOffset.x, this.previousCameraOffset.y);
-      console.log('getEventLocation', e.clientX, e.clientY);
-      this.cameraOffset.x = (getEventLocation(e).x - (this.dragStart.x + this.previousCameraOffset.x));
-      this.cameraOffset.y = (getEventLocation(e).y - (this.dragStart.y + this.previousCameraOffset.y));
-      console.log('current (result):',this.cameraOffset.x, this.cameraOffset.y);      
-    }
+      this.cameraOffset.x = (getEventLocation(e, this.canvas).x - (this.dragStart.x));
+      this.cameraOffset.y = (getEventLocation(e, this.canvas).y - (this.dragStart.y));
+     }
   }
 }
 
-function getEventLocation(e: MouseEvent): any{
+function getEventLocation(e: MouseEvent, canvas: HTMLCanvasElement): any{
   if(e.clientX && e.clientY){
-    return {x: e.clientX, y: e.clientY};      
+    const canvasRelavitveBound = canvas.getBoundingClientRect();
+    return {x: e.clientX - canvasRelavitveBound.left, y: e.clientY - canvasRelavitveBound.top};      
   }
 }
